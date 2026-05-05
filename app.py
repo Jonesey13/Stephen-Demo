7754662b0ef51860567d9e7ba64b2cdb11ec4a06
Alex Jones <alexander.jones@corndel.com>
2026-05-05 14:58:57 +0000

    Add second page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/index.html")
+@app.route("/")
 def homepage():
     return render_template("index.html")
 
-@app.route("/avd_details/<avd_id>")
-def avd_details(avd_id):
-    return 
+@app.route("/avd_details.html")
+def avd_details():
+    return render_template("avd_details.html")
